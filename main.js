8c58fb07cd81222381ea402a5dda4029fca5eaa5
Pauline Oudeman <pauline.van.der.kraats@mendix.com>
2015-03-27 11:59:28 +0100

    Not opening the preview window on startup brackets

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ define(function (require, exports, module) {
 
                     // Create the workshop snippets.
                     MendixWorkshopManager.setupSnippets();
-                    MendixWorkshopManager.panel.show();
 
                     /**
                      * Mendix Workspace Manager.
